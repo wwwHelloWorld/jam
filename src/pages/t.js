09f133279dc471e7e7f5b0aa2902f6f6a5cd0c25
SashaRudenko <for_london@ukr.net>
2023-06-27 22:45:58 +0300

    add video redirect: fix

---
+++ src/pages/t.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+const VideoPlayer = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://rr4---sn-4gxxoxu-3c2l.googlevideo.com/videoplayback?expire=1687899350&ei=dviaZILwNcKilu8Pjt668Ac&ip=181.214.164.29&id=o-AGyVCeMiVU_CkrJ0O5QKaik_k4sekeEFrbuTcaH9cRhT&itag=18&source=youtube&requiressl=yes&spc=qEK7B3i-hLClXDqJhxCmu2YOIFZPZ4BHI-_v1Xbl7Q&vprv=1&svpuc=1&mime=video%2Fmp4&ns=CrOchT278mJ4z3lfERV1tbEN&cnr=14&ratebypass=yes&dur=212.091&lmt=1674233743350828&fexp=24007246,24472446,51000011,51000013&beids=24472446&c=WEB&txp=4530434&n=EqkI7Mr_kue8FA&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRAIgDyuGVcE0DjLxKMOwNkkqdyW4monqUYWTIpSUWpLIvt8CIC4Y4rQPJJT9jj1WVzZRIvv-mAVW4zWC2-uvfRX7m0F0&redirect_counter=1&rm=sn-vgqely7s&req_id=9536aa6ea0dda3ee&cms_redirect=yes&ipbypass=yes&mh=7c&mip=46.219.111.230&mm=31&mn=sn-4gxxoxu-3c2l&ms=au&mt=1687878826&mv=m&mvi=4&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhAKnzcP8T1ovMh7W3r77oVnQleRZFHX4C6TbFPMvrGhLqAiAxmZQbFJkiU20rTODi-9f4CmmunV4JYvcSAi_txItS7A%3D%3D"
+  }, [])
+
+  return null
+}
+
+export default VideoPlayer
